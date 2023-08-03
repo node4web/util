709d49f399da3dec053d2c29e93d76650960ed41
Jacob Hummer <jcbhmr@outlook.com>
2023-08-03 03:03:43 +0000

    always import to satisfy vitest

--- src/toUSVString-default.ts
+++ src/toUSVString-default.ts
@@ -1,11 +1,12 @@
+import StringPrototypeToWellFormed from "./lib/StringPrototypeToWellFormed.js";
+
 let toUSVString: (s: string) => string;
 // @ts-ignore
 if (String.prototype.toWellFormed) {
   // @ts-ignore
   toUSVString = Object.call.bind(String.prototype.toWellFormed);
 } else {
-  toUSVString =
-    require("./lib/StringPrototypeToWellFormed.js") as typeof import("./lib/StringPrototypeToWellFormed.js");
+  toUSVString = StringPrototypeToWellFormed;
 }
 
 export = toUSVString;
